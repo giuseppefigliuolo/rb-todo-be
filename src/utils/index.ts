a14e57293837e3b2a710dcf9bb121d4877d9df3e
Giuseppe Figliuolo <giuseppe.figliuolo@bitrock.it>
2024-10-01 12:25:45 +0200

    ts error type fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -19,118 +19,104 @@ export const loadTodos = async (): Promise<ITodo[]> => {
 }
 
 export const saveTodos = async (todo: ITodo) => {
-  fs.readFile(
-    filePath,
-    'utf8',
-    (err: NodeJS.ErrnoException | null, data: string) => {
-      if (err) {
-        console.error('Error reading the file:', err)
-        return
-      }
-
-      try {
-        let jsonArray: ITodo[] = data ? JSON.parse(data) : []
-
-        jsonArray.push(todo)
-
-        fs.writeFile(
-          filePath,
-          JSON.stringify(jsonArray, null, 2),
-          'utf8',
-          (err: NodeJS.ErrnoException | null) => {
-            if (err) {
-              console.error('Error writing to the file:', err)
-              return
-            }
-            console.log(
-              'Successfully added the new object and updated the file!'
-            )
-          }
-        )
-      } catch (parseError) {
-        console.error('Error parsing JSON data:', parseError)
-      }
+  fs.readFile(filePath, 'utf8', (err: Error | null, data: string) => {
+    if (err) {
+      console.error('Error reading the file:', err)
+      return
     }
-  )
+
+    try {
+      let jsonArray: ITodo[] = data ? JSON.parse(data) : []
+
+      jsonArray.push(todo)
+
+      fs.writeFile(
+        filePath,
+        JSON.stringify(jsonArray, null, 2),
+        'utf8',
+        (err: Error | null) => {
+          if (err) {
+            console.error('Error writing to the file:', err)
+            return
+          }
+          console.log('Successfully added the new object and updated the file!')
+        }
+      )
+    } catch (parseError) {
+      console.error('Error parsing JSON data:', parseError)
+    }
+  })
 }
 
 export const deleteTodo = async (id: string) => {
-  fs.readFile(
-    filePath,
-    'utf8',
-    (err: NodeJS.ErrnoException | null, data: string) => {
-      if (err) {
-        console.error('Error reading the file:', err)
+  fs.readFile(filePath, 'utf8', (err: Error | null, data: string) => {
+    if (err) {
+      console.error('Error reading the file:', err)
+      return
+    }
+
+    try {
+      const jsonArray: ITodo[] = JSON.parse(data)
+
+      const index = jsonArray.findIndex((t) => t.id === id)
+      if (index === -1) {
         return
       }
 
-      try {
-        const jsonArray: ITodo[] = JSON.parse(data)
+      jsonArray.splice(index, 1)
 
-        const index = jsonArray.findIndex((t) => t.id === id)
-        if (index === -1) {
-          return
-        }
-
-        jsonArray.splice(index, 1)
-
-        fs.writeFile(
-          filePath,
-          JSON.stringify(jsonArray, null, 2),
-          'utf8',
-          (err: NodeJS.ErrnoException | null) => {
-            if (err) {
-              console.error('Error writing to the file:', err)
-              return
-            }
-            console.log('Successfully removed the object and updated the file!')
+      fs.writeFile(
+        filePath,
+        JSON.stringify(jsonArray, null, 2),
+        'utf8',
+        (err: Error | null) => {
+          if (err) {
+            console.error('Error writing to the file:', err)
+            return
           }
-        )
-      } catch (parseError) {
-        console.error('Error parsing JSON data:', parseError)
-      }
+          console.log('Successfully removed the object and updated the file!')
+        }
+      )
+    } catch (parseError) {
+      console.error('Error parsing JSON data:', parseError)
     }
-  )
+  })
 }
 
 export const updateTodo = async (id: string, todo: ITodo) => {
-  fs.readFile(
-    filePath,
-    'utf8',
-    (err: NodeJS.ErrnoException | null, data: string) => {
-      if (err) {
-        console.error('Error reading the file:', err)
+  fs.readFile(filePath, 'utf8', (err: Error | null, data: string) => {
+    if (err) {
+      console.error('Error reading the file:', err)
+      return
+    }
+
+    try {
+      const jsonArray: ITodo[] = JSON.parse(data)
+
+      const index = jsonArray.findIndex((t) => t.id === id)
+      if (index === -1) {
+        console.error('Todo with id', id, 'does not exist.')
         return
       }
 
-      try {
-        const jsonArray: ITodo[] = JSON.parse(data)
+      jsonArray[index] = todo
 
-        const index = jsonArray.findIndex((t) => t.id === id)
-        if (index === -1) {
-          console.error('Todo with id', id, 'does not exist.')
-          return
-        }
-
-        jsonArray[index] = todo
-
-        fs.writeFile(
-          filePath,
-          JSON.stringify(jsonArray, null, 2),
-          'utf8',
-          (err: NodeJS.ErrnoException | null) => {
-            if (err) {
-              console.error('Error writing to the file:', err)
-              return
-            }
-            console.log('Successfully updated the object and updated the file!')
+      fs.writeFile(
+        filePath,
+        JSON.stringify(jsonArray, null, 2),
+        'utf8',
+        (err: NodeJS.ErrnoException | null) => {
+          if (err) {
+            console.error('Error writing to the file:', err)
+            return
           }
-        )
-      } catch (parseError) {
-        console.error('Error parsing JSON data:', parseError)
-      }
+          console.log('Successfully updated the object and updated the file!')
+        }
+      )
+    } catch (parseError) {
+      console.error('Error parsing JSON data:', parseError)
     }
-  )
+  })
 }
 
 export const isValidTodo = (data: unknown): boolean => {
